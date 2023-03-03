94aefff3825f85e2e57da99c79f2ff588d790bda
Sumit <ksumit1109@gmail.com>
2023-03-03 23:02:47 +0530

    Task 3 OF DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("task2 of DOM")
+var title = document.getElementById("main-header");
+title.style.borderBottom ="solid 5px #000";
+var Element = document.querySelectorAll('h2')
+console.log(Element[0])
+Element[0].style.color = "green";
+Element[0].style.fontWeight = "bold";
+
+// Element.style.FontWeight = "bold"
+// Element.style.color = "green"
