62c525791abef9a76e917f1738b90b3f672b74fb
Sumit <ksumit1109@gmail.com>
2023-03-04 23:26:45 +0530

    task 5

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 // console.log("task2 of DOM")
-var title = document.getElementById("main-header");
-title.style.borderBottom ="solid 5px #000";
-var Element = document.querySelectorAll('h2')
-// console.log(Element[0])
-Element[0].style.color = "green";
-Element[0].style.fontWeight = "bold";
+// var title = document.getElementById("main-header");
+// title.style.borderBottom ="solid 5px #000";
+// var Element = document.querySelectorAll('h2')
+// // console.log(Element[0])
+// Element[0].style.color = "green";
+// Element[0].style.fontWeight = "bold";
 
 // Element.style.FontWeight = "bold"
 // Element.style.color = "green"
-var Bg_green = document.getElementsByClassName("list-group-item");
-Bg_green[2].style.backgroundColor = "green"; 
 
-var boldElement = document.getElementsByClassName("list-group-item");
-for(var i=0;i<boldElement.length;i++){
-    boldElement[i].style.fontWeight = "bold";
-}
+// Task 4 
+// var Bg_green = document.getElementsByClassName("list-group-item");
+// Bg_green[2].style.backgroundColor = "green"; 
+
+// var boldElement = document.getElementsByClassName("list-group-item");
+// for(var i=0;i<boldElement.length;i++){
+//     boldElement[i].style.fontWeight = "bold";
+// }
+
+// // Task 5
+var Edit = document.getElementsByClassName("task")
+Edit[0].style.fontWeight = "bold";
+
+var Edit2 = document.getElementsByTagName("li");
+console.log(Edit2)
+Edit2[4].style.backgroundColor = "grey";
+
