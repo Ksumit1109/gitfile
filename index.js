def1f5c6612b1e484806778a19657dc68ba7ca1f
Sumit <ksumit1109@gmail.com>
2023-03-04 20:02:47 +0530

    task4 bold the list items and change the bgcolor

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-console.log("task2 of DOM")
+// console.log("task2 of DOM")
 var title = document.getElementById("main-header");
 title.style.borderBottom ="solid 5px #000";
 var Element = document.querySelectorAll('h2')
-console.log(Element[0])
+// console.log(Element[0])
 Element[0].style.color = "green";
 Element[0].style.fontWeight = "bold";
 
 // Element.style.FontWeight = "bold"
 // Element.style.color = "green"
+var Bg_green = document.getElementsByClassName("list-group-item");
+Bg_green[2].style.backgroundColor = "green"; 
+
+var boldElement = document.getElementsByClassName("list-group-item");
+for(var i=0;i<boldElement.length;i++){
+    boldElement[i].style.fontWeight = "bold";
+}
