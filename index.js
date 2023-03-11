5ea595572b9559fc147cc1bd0503068309bca494
Sumit <ksumit1109@gmail.com>
2023-03-11 20:01:11 +0530

    Task 8

--- index.js
+++ index.js
@@ -35,28 +35,67 @@
 
 // Task 7
 
-var NewDiv = document.createElement('div');
-NewDiv.className= 'NewClass';
-NewDiv.id = 'NewId';
-NewDiv.setAttribute('title', 'Hii Div')
+// var NewDiv = document.createElement('div');
+// NewDiv.className= 'NewClass';
+// NewDiv.id = 'NewId';
+// NewDiv.setAttribute('title', 'Hii Div')
 
 
-var textNode = document.createTextNode('HEllo word');
+// var textNode = document.createTextNode('HEllo word');
 
-NewDiv.appendChild(textNode);
-var headercontainer = document.querySelector('header .container');
-var headerH1 = document.querySelector('header h1');
+// NewDiv.appendChild(textNode);
+// var headercontainer = document.querySelector('header .container');
+// var headerH1 = document.querySelector('header h1');
 
-headercontainer.insertBefore(NewDiv,headerH1);
+// headercontainer.insertBefore(NewDiv,headerH1);
 
-// Question 2 
-var NewDiv2 = document.createElement('li');
-var textNode2 = document.createTextNode('HEllo word');
-NewDiv2.appendChild(textNode2);
-var Items = document.getElementById('items');
-NewDiv2.className= "list-group-item";
-NewDiv2.appendChild(textNode2);
-Items.insertBefore(NewDiv2 , Items.childNodes[0])
+// // Question 2 
+// var NewDiv2 = document.createElement('li');
+// var textNode2 = document.createTextNode('HEllo word');
+// NewDiv2.appendChild(textNode2);
+// var Items = document.getElementById('items');
+// NewDiv2.className= "list-group-item";
+// NewDiv2.appendChild(textNode2);
+// Items.insertBefore(NewDiv2 , Items.childNodes[0])
 
-console.log(NewDiv);
+// console.log(NewDiv);
 
+// Task 8
+var SelectForm = document.getElementById('addForm');
+var SelectItem = document.getElementById('items');
+
+SelectForm.addEventListener('submit', additem);
+SelectItem.addEventListener('click' , removeitem);
+
+function additem(e){
+    e.preventDefault();
+    var NewItem = document.getElementById('item').value;
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(NewItem));
+    SelectItem.appendChild(li);
+
+    var Delbtn = document.createElement('button');
+    Delbtn.className = 'btn btn-danger btn-sm float-right delete';
+    Delbtn.appendChild(document.createTextNode('X'));
+    li.appendChild(Delbtn);
+}
+
+
+function removeitem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+            var li = e.target.parentElement;
+            SelectItem.removeChild(li);
+        }
+    }
+}
+
+ // Add Edit button 
+ var list = document.querySelector('li')
+ var EditBtn = document.createElement('button');
+ EditBtn.className = 'btn btn-primary btn-sm float-right'
+ EditBtn.appendChild(document.createTextNode('Edit'));
+ EditBtn.style.marginRight = "4px"
+ list.appendChild(EditBtn);   
+ console.log(EditBtn)
