ef458fea736ef76adb053a793a6bf2874d77beac
Sumit <ksumit1109@gmail.com>
2023-05-28 16:18:16 +0530

    Deleting the Appointments task 19 CRUD

--- Appointment page/CRUD.js
+++ Appointment page/CRUD.js
@@ -1,6 +1,5 @@
-
 window.addEventListener('DOMContentLoaded', function () {
-    axios.get("https://crudcrud.com/api/36cd35ff531e405aa90635740ce0bcf8/userdata")
+    axios.get("https://crudcrud.com/api/b838b352eea945f3b5ba2e5de9d1d480/userdata")
       .then((response) => {
         console.log(response);
         for (var i = 0; i < response.data.length; i++) {
@@ -12,47 +11,98 @@ window.addEventListener('DOMContentLoaded', function () {
       });
   });
   
-  var selectform = document.getElementById('form');
   var list22 = document.getElementById('items1');
-  selectform.addEventListener('submit', items);
   
-  function items(e) {
-    e.preventDefault();
-    var Name = document.getElementById('name').value;
-    var Email = document.getElementById('email').value;
-    var Contact = document.getElementById('contact').value;
+  function createUserDetail(Name, Email, Contact) {
     var li = document.createElement("li");
     li.className = "listItems";
     li.appendChild(document.createTextNode(Name + ' - ' + Email + ' - ' + Contact));
-    list22.appendChild(li);
   
     // CREATE DELETE BUTTON
     var delBtn = document.createElement('button');
     delBtn.className = "Del";
     delBtn.appendChild(document.createTextNode('Delete'));
-    li.appendChild(delBtn);
-    delBtn.onclick = () => {
-      localStorage.removeItem(Email);
+    delBtn.onclick = function () {
+      deleteUserData(Email);
       list22.removeChild(li);
     };
     li.appendChild(delBtn);
-    list22.appendChild(li);
   
     // CREATE EDIT BUTTON
     var editBtn = document.createElement('button');
     editBtn.className = 'edit';
     editBtn.appendChild(document.createTextNode('Edit'));
     editBtn.style.margin = '4px';
+    editBtn.onclick = function () {
+      editUserDetail(Name, Email, Contact, li);
+    };
     li.appendChild(editBtn);
   
-    editBtn.onclick = () => {
-      localStorage.removeItem(Email);
-      document.getElementById('name').value = Name;
-      document.getElementById('email').value = Email;
-      document.getElementById('contact').value = Contact;
+    list22.appendChild(li);
+  }
+  
+  function deleteUserData(user) {
+    axios.get(`https://crudcrud.com/api/b838b352eea945f3b5ba2e5de9d1d480/userdata?email=${user}`)
+      .then((response) => {
+        const id = response.data[0]._id;
+        axios.delete(`https://crudcrud.com/api/b838b352eea945f3b5ba2e5de9d1d480/userdata/${id}`)
+          .then(() => {
+            console.log("User detail deleted successfully");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
+  function editUserDetail(Name, Email, Contact, listItem) {
+    localStorage.removeItem(Email);
+    document.getElementById('name').value = Name;
+    document.getElementById('email').value = Email;
+    document.getElementById('contact').value = Contact;
+    list22.removeChild(listItem);
+  }
+  
+  function displayUserDetails(data) {
+    var { Name, Email, Contact } = data;
+    var li = document.createElement("li");
+    li.className = "listItems";
+    li.appendChild(document.createTextNode(Name + ' - ' + Email + ' - ' + Contact));
+  
+    // CREATE DELETE BUTTON
+    var delBtn = document.createElement('button');
+    delBtn.className = "Del";
+    delBtn.appendChild(document.createTextNode('Delete'));
+    delBtn.onclick = function () {
+      deleteUserData(Email);
       list22.removeChild(li);
     };
+    li.appendChild(delBtn);
+  
+    // CREATE EDIT BUTTON
+    var editBtn = document.createElement('button');
+    editBtn.className = 'edit';
+    editBtn.appendChild(document.createTextNode('Edit'));
+    editBtn.style.margin = '4px';
+    editBtn.onclick = function () {
+      editUserDetail(Name, Email, Contact, li);
+    };
+    li.appendChild(editBtn);
+  
     list22.appendChild(li);
+  }
+  
+  var selectform = document.getElementById('form');
+  selectform.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var Name = document.getElementById('name').value;
+    var Email = document.getElementById('email').value;
+    var Contact = document.getElementById('contact').value;
+  
+    createUserDetail(Name, Email, Contact);
   
     var storage = {
       Name,
@@ -60,7 +110,7 @@ window.addEventListener('DOMContentLoaded', function () {
       Contact
     };
   
-    axios.post("https://crudcrud.com/api/36cd35ff531e405aa90635740ce0bcf8/userdata", storage)
+    axios.post("https://crudcrud.com/api/b838b352eea945f3b5ba2e5de9d1d480/userdata", storage)
       .then((response) => {
         console.log(response);
       })
@@ -70,38 +120,5 @@ window.addEventListener('DOMContentLoaded', function () {
   
     var convertToString = JSON.stringify(storage);
     localStorage.setItem(Email, convertToString);
-  }
-  function displayUserDetails(data) {
-    var li = document.createElement("li");
-    li.className = "listItems";
-    li.appendChild(document.createTextNode(data.Name + ' - ' + data.Email + ' - ' + data.Contact));
-    list22.appendChild(li);
-
-    var delBtn = document.createElement('button');
-    delBtn.className = "Del";
-    delBtn.appendChild(document.createTextNode('Delete'));
-    li.appendChild(delBtn);
-    delBtn.onclick = () => {
-      localStorage.removeItem(Email);
-      list22.removeChild(li);
-    };
-    li.appendChild(delBtn);
-    list22.appendChild(li);
+   });
   
-    // CREATE EDIT BUTTON
-    var editBtn = document.createElement('button');
-    editBtn.className = 'edit';
-    editBtn.appendChild(document.createTextNode('Edit'));
-    editBtn.style.margin = '4px';
-    li.appendChild(editBtn);
-  
-    editBtn.onclick = () => {
-      localStorage.removeItem(Email);
-      document.getElementById('name').value = Name;
-      document.getElementById('email').value = Email;
-      document.getElementById('contact').value = Contact;
-      list22.removeChild(li);
-    };
-    list22.appendChild(li);
-
-  }
